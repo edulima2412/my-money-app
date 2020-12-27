f3c47ebcec3caab80aa30435f7c880cc100fba97
Eduardo Lima <edulima2412@gmail.com>
2020-12-27 19:05:06 -0300

    alter user aut

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -3,11 +3,12 @@ import { Router, Route, IndexRoute , Redirect, hashHistory } from "react-router"
 
 import Dashboard from "../dashboard/dashboard";
 import BillingCycle from "../billingCycle/billingCycle";
-import App from "./app";
+import AuthOrApp from './authOrApp'
+
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={App}>
+        <Route path='/' component={AuthOrApp}>
             <IndexRoute component={Dashboard} />
             <Route path='billingCycles' component={BillingCycle} />
         </Route>
