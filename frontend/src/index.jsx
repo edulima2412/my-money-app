53b7cc577025ae9c3c2f00ef36d8c85250af3aaa
Eduardo Lima <edulima2412@gmail.com>
2020-12-27 16:06:20 -0300

    bugfix routes

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,15 +7,15 @@ import promise from "redux-promise";
 import multi from "redux-multi";
 import thunk from "redux-thunk";
 
-import App from "./main/app";
 import reducers from "./main/reducers";
+import Routes from "./main/routes";
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Routes />
     </Provider>,
     document.getElementById('app')
 )
